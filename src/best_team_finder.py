d7e40d25294beda3d3a6a0ec179ec2766b5cefe3
malfredk <malenekf95@gmail.com>
2025-01-12 17:40:17 +0100

    refactoring

--- src/best_team_finder.py
+++ src/best_team_finder.py
@@ -1,17 +1,14 @@
-from file_utils import read_names_from_file, read_team_members_from_file
+from file_utils import create_list_from_file, read_team_members_from_file
 from team_utils import NUMBER_OF_DRIVERS, NUMBER_OF_CONSTRUCTORS, Team
 from itertools import combinations
 
 def determine_best_team(
-        drivers_file_path: str, 
-        constructors_file_path: str, 
+        drivers, 
+        constructors, 
         budget: float, 
-        previous_race_team_file_path: str,
+        previous_team_names,
         free_transfers: int
         ):
-    drivers = read_team_members_from_file(drivers_file_path)
-    constructors = read_team_members_from_file(constructors_file_path)
-    previous_team_names = read_names_from_file(previous_race_team_file_path)
 
     drivers_combos = list(combinations(drivers, NUMBER_OF_DRIVERS))
     constructors_combos = list(combinations(constructors, NUMBER_OF_CONSTRUCTORS))
